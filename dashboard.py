48da54760c89473829af7d85e7d2fcc15ba435fb
gemgalmyp <mylarypales@gmail.com>
2025-12-27 03:25:11 +0800

    Initial commit

---
+++ dashboard.py
@@ -0,0 +1,22 @@
+from tkinter import * 
+
+
+window = Tk()
+
+window.title("ARSE ELECTRONICS IMS")
+window.geometry("1420x810+0+0")
+window.resizable(0,0)
+window.config(bg="#bdbdbd")
+
+
+titleLabel = Label(
+    window, 
+    text="ARSE Inventory Management System", 
+    font=("Franklin Gothic Book (Headings)", 40, "bold"), 
+    bg="#045c19", 
+    fg="#d2db70"
+)
+
+       
+
+window.mainloop()
