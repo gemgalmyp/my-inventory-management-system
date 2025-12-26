6606f9eb12a3f848fc3b29bff431bb3e45b20c91
gemgalmyp <mylarypales@gmail.com>
2025-12-27 04:38:19 +0800

    Fixed the dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,6 @@
-from tkinter import * 
-
+import os
+from tkinter import *
+from tkinter import PhotoImage
 
 window = Tk()
 
@@ -8,9 +9,26 @@ window.geometry("1420x810+0+0")
 window.resizable(0,0)
 window.config(bg="#bdbdbd")
 
-# bg_image = PhotoImage(file="Inventory.jpg")
-titleLabel = Label(window, text="ARSE Inventory Management System", font=("Arial", 40, "bold"), bg="#045c19", fg="#d2db70")
+base_dir = os.path.dirname(os.path.abspath(__file__))
+icon_path = os.path.join(base_dir, "icon.png")   # put icon.png next to dashboard.py
 
-# titleLabel.image = bg_image         
+icon = PhotoImage(file=icon_path)
+window.iconphoto(True, icon)
+window._icon = icon   # keep reference to avoid GC
+window.iconphoto(True, icon)
+titleLabel = Label(
+    window, 
+    image=icon,
+    compound=LEFT,
+    text="ARSE Inventory Management System",
+    font=("Franklin Gothic Book (Headings)", 40, "bold"), 
+    bg="#045c19", 
+    fg="#d2db70",
+    padx=10
+)
+
+titleLabel.image = icon
+titleLabel.place(x=0, y=0)
+        
 
 window.mainloop()
