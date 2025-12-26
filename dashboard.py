80a5997f94790b0c566f29718ef6772932907519
gemgalmyp <mylarypales@gmail.com>
2025-12-27 03:32:39 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,15 +8,16 @@ window.geometry("1420x810+0+0")
 window.resizable(0,0)
 window.config(bg="#bdbdbd")
 
-
+# bg_image = PhotoImage(file="Inventory.jpg")
 titleLabel = Label(
-    window, 
+    window,
+     # image=bg_image,
     text="ARSE Inventory Management System", 
     font=("Franklin Gothic Book (Headings)", 40, "bold"), 
     bg="#045c19", 
     fg="#d2db70"
 )
 
-       
+# titleLabel.image = bg_image         
 
 window.mainloop()
