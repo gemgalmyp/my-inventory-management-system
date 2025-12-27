1c8c452cdf2776260190e8734f184b66c88c37e2
gemgalmyp <mylarypales@gmail.com>
2025-12-27 13:08:56 +0800

    Creating card frame

--- dashboard.py
+++ dashboard.py
@@ -21,7 +21,7 @@ titleLabel = Label(
     compound=LEFT,
     text="ARSE Inventory Management System",
     font=("Franklin Gothic Book (Headings)", 40, "bold"), 
-    bg="#045c19", 
+    bg="#045517", 
     fg="#d2db70",
     padx=10,
     anchor="w"
@@ -60,6 +60,7 @@ imageLabel = Label(leftFrame, image=logoImage)
 imageLabel.place(x=0, y=5, width=300, height=200)
 # imageLabel.pack(fill=BOTH)
 
+# DESIGN OF THE DASHBOARD LEFT FRAME
 menuLabel = Label(
     leftFrame,
     text="Menu",
@@ -146,5 +147,169 @@ ExitButton = Button(
     padx=40
 )
 ExitButton.place(x=0, y=595, width=300, height=60)
+
+# DASHBOARD CONTENTS
+# Total Employees
+emp_frame = Frame(
+    window,
+    bg="#1e8637",
+    bd = 3,
+    relief=RIDGE,
+    highlightthickness=5,
+    highlightbackground="#045517"
+)
+emp_frame.place(x=500, y=150, width=280, height=170)
+total_emp_icon = PhotoImage(file="total_employees.png")
+total_emp_icon_label = Label(emp_frame, image=total_emp_icon, bg="#1e8637")
+total_emp_icon_label.pack()
+
+total_emp_label = Label(
+    emp_frame, 
+    text="Total Employees", 
+    bg="#1e8637", 
+    fg="white", 
+    font=("Franklin Gothic Book (Headings)", 16, "bold")
+)
+total_emp_label.pack()
+
+total_emp_count_label = Label(
+    emp_frame, 
+    text="0", 
+    bg="#1e8637", 
+    fg="white", 
+    font=("Franklin Gothic Book (Headings)", 30, "bold")
+)
+total_emp_count_label.pack()
+
+# Total Suppliers
+sup_frame = Frame(
+    window,
+    bg="#1e8637",
+    bd = 3,
+    relief=RIDGE,
+    highlightthickness=5,
+    highlightbackground="#045517"
+)
+sup_frame.place(x=950, y=150, width=280, height=170)
+total_sup_icon = PhotoImage(file="total_suppliers.png")
+total_sup_icon_label = Label(sup_frame, image=total_sup_icon, bg="#1e8637")
+total_sup_icon_label.pack()
+
+total_sup_label = Label(
+    sup_frame, 
+    text="Total Suppliers", 
+    bg="#1e8637", 
+    fg="white", 
+    font=("Franklin Gothic Book (Headings)", 16, "bold")
+)
+total_sup_label.pack()
+
+total_sup_count_label = Label(
+    sup_frame, 
+    text="0", 
+    bg="#1e8637", 
+    fg="white", 
+    font=("Franklin Gothic Book (Headings)", 30, "bold")
+)
+total_sup_count_label.pack()
+
+# Total Categories
+cat_frame = Frame(
+    window,
+    bg="#1e8637",
+    bd = 3,
+    relief=RIDGE,
+    highlightthickness=5,
+    highlightbackground="#045517"
+)
+cat_frame.place(x=500, y=390, width=280, height=170)
+total_cat_icon = PhotoImage(file="total_categories.png")
+total_cat_icon_label = Label(cat_frame, image=total_cat_icon, bg="#1e8637")
+total_cat_icon_label.pack()
+
+total_cat_label = Label(
+    cat_frame, 
+    text="Total Categories", 
+    bg="#1e8637", 
+    fg="white", 
+    font=("Franklin Gothic Book (Headings)", 16, "bold")
+)
+total_cat_label.pack()
+
+total_cat_count_label = Label(
+    cat_frame, 
+    text="0", 
+    bg="#1e8637", 
+    fg="white", 
+    font=("Franklin Gothic Book (Headings)", 30, "bold")
+)
+total_cat_count_label.pack()
+
+# Total Products
+prod_frame = Frame(
+    window,
+    bg="#1e8637",
+    bd = 3,
+    relief=RIDGE,
+    highlightthickness=5,
+    highlightbackground="#045517"
+)
+prod_frame.place(x=950, y=390, width=280, height=170)
+total_prod_icon = PhotoImage(file="total_products.png")
+total_prod_icon_label = Label(prod_frame, image=total_prod_icon, bg="#1e8637")
+total_prod_icon_label.pack()
+
+total_prod_label = Label(
+    prod_frame, 
+    text="Total Products", 
+    bg="#1e8637", 
+    fg="white", 
+    font=("Franklin Gothic Book (Headings)", 16, "bold")
+)
+total_prod_label.pack()
+
+total_prod_count_label = Label(
+    prod_frame, 
+    text="0", 
+    bg="#1e8637", 
+    fg="white", 
+    font=("Franklin Gothic Book (Headings)", 30, "bold")
+)
+total_prod_count_label.pack()
+
+# Total Sales
+sales_frame = Frame(
+    window,
+    bg="#1e8637",
+    bd = 3,
+    relief=RIDGE,
+    highlightthickness=5,
+    highlightbackground="#045517"
+)
+sales_frame.place(x=720, y=610, width=280, height=170)
+total_sales_icon = PhotoImage(file="total_sales.png")
+total_sales_icon_label = Label(sales_frame, image=total_sales_icon, bg="#1e8637")
+total_sales_icon_label.pack()
+
+total_sales_label = Label(
+    sales_frame, 
+    text="Total Sales", 
+    bg="#1e8637", 
+    fg="white", 
+    font=("Franklin Gothic Book (Headings)", 16, "bold")
+)
+total_sales_label.pack()
+
+total_sales_count_label = Label(
+    sales_frame, 
+    text="0", 
+    bg="#1e8637", 
+    fg="white", 
+    font=("Franklin Gothic Book (Headings)", 30, "bold")
+)
+total_sales_count_label.pack()
+
+
+
 
 window.mainloop()
